d56ac9127aaf67746f16a47786a724d37d4d33e6
rcskinner <ryan.skinner@datadoghq.com>
2025-03-31 13:59:14 -0600

    Created the vpc / gke modules for the terraform script

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,18 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = google_compute_network.vpc.id
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = google_compute_subnetwork.subnet.id
+}
+
+output "subnet_cidr" {
+  value = google_compute_subnetwork.subnet.ip_cidr_range
+}
+
+output "network_name" {
+  description = "The name of the VPC network"
+  value       = google_compute_network.vpc.name
+} 
