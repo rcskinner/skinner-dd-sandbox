8639da7ffa4a7b92b72007cba69cd112ec74f58f
rcskinner <ryan.skinner@datadoghq.com>
2025-03-31 15:18:11 -0600

    Fun™ tweaks to the GKE deployment for Container Optimized OS

---
+++ dd-agent/gke/gke-standard-setup.sh
@@ -0,0 +1,13 @@
+#/bin/bash 
+
+# Create the Datadog API Secret 
+read -sp "Enter your API key: " API_KEY
+echo 
+
+# Update Helm
+helm repo add datadog https://helm.datadoghq.com
+
+#required for introspection.enabled=true required for some reason
+helm install datadog-operator datadog/datadog-operator --set introspection.enabled=true -n datadog
+kubectl create secret generic datadog-secret --from-literal api-key=$API_KEY -n datadog
+k apply -f datadog-values.yaml
