8fcf32938ef9f6ccbdbde98ed29b98f583d6d132
rcskinner <ryan.skinner@datadoghq.com>
2025-04-02 07:23:21 -0600

    fixed the setup script

--- dd-agent/gke/gke-standard-setup.sh
+++ dd-agent/gke/gke-standard-setup.sh
@@ -11,4 +11,4 @@ kubectl create namespace datadog
 #required for introspection.enabled=true required for some reason
 helm install datadog-operator datadog/datadog-operator --set introspection.enabled=true -n datadog
 kubectl create secret generic datadog-secret --from-literal api-key=$API_KEY -n datadog
-k apply -f datadog-values.yaml
+kubectl apply -f datadog-values.yaml
